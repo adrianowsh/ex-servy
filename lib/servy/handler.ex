bee6420a2cac2a228f84bb897334b27b1983e4b3
Adriano Mota <adrianowsh@gmail.com>
2024-07-24 19:09:08 -0300

    uaing multiclause functions

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -46,6 +46,17 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bears #{id} "}
   end
 
+  def route(%{method: "GET", path: "/about"} = conv) do
+    Path.expand("../../lib/pages", __DIR__)
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{conv | status: 403, resp_body: "Bears must never be deleted!"}
+  end
+
   def route(%{method: _method, path: path} = conv) do
     %{conv | status: 404, resp_body: "No #{path} gere!"}
   end
@@ -54,7 +65,7 @@ defmodule Servy.Handler do
     """
     HTTP/1.1 #{status} #{status_reason(status)}
     Content-Type: text/html
-    Content-Length: #{String.length(body)}
+    Content-Length: #{byte_size(body)}
 
     #{body}
     """
@@ -72,6 +83,15 @@ defmodule Servy.Handler do
 
     codes[code]
   end
+
+  defp handle_file({:ok, content}, conv),
+    do: %{conv | status: 200, resp_body: content}
+
+  defp handle_file({:error, :enoent}, conv),
+    do: %{conv | status: 404, resp_body: "File not found"}
+
+  defp handle_file({:error, reason}, conv),
+    do: %{conv | status: 500, resp_body: "File error: #{reason}"}
 end
 
 request = """
@@ -124,6 +144,30 @@ IO.puts(response)
 
 request = """
 GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+DELETE /bears/2 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
